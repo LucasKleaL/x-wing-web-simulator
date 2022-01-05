43d698463acacd259c97bf3d6dd105d41ada5b85
LucasKleaL <lucaskleal222@outlook.com>
2022-01-05 12:42:20 -0300

    X-Wing 0.0
    -Adicionado interface básica do jogo, com os botões de controle e start, barra de vida e indicadores do objetivo e tempo até o fim do turno.

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function() {
+
+    //
+
+});
+
+function startGame() {
+    
+    $("#controlButton1").removeClass("hidden-control-button").addClass("control-button");
+    $("#controlButton2").removeClass("hidden-control-button").addClass("control-button");
+    $("#controlButton3").removeClass("hidden-control-button").addClass("control-button");
+    $("#startButton").removeClass("start-button").addClass("hidden-start-button");
+
+}
+
+function atirar() {
+    //
+}
+
+function desviar() {
+    //
+}
+
+function acelerar() {
+    //
+}
